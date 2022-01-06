410dd8500a3a8ffe31ff1ec2caa9319858413435
Chris Boyle <cthboyle6@gmail.com>
2022-01-06 09:06:09 -0500

    added form submit handler in AddUser component, page background style color dark grey

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,14 @@
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    props.onUserSubmit();
+  };
+
   return (
-    <form>
+    <form onSubmit={addUserHandler}>
       <div className={styles.input}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
