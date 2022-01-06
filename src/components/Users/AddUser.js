e811323baf59ba25ebebfceb38b0e1db729f9504
Chris Boyle <cthboyle6@gmail.com>
2022-01-06 09:22:16 -0500

    Card styling, wrapped AddUser component and connected styles

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,7 @@
 import styles from "./AddUser.module.css";
 
+import Card from "../UI/Card";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
@@ -8,17 +10,15 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <div className={styles.input}>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
-      </div>
-      <div className={styles.input}>
         <label htmlFor="age">Age</label>
         <input id="age" type="number"></input>
-      </div>
-      <button type="submit">Add User</button>
-    </form>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
