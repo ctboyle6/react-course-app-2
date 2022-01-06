0e587e469a543ada7741d7f2c46bd6fe91db600a
Chris Boyle <cthboyle6@gmail.com>
2022-01-06 10:40:40 -0500

    connected AddUser submit to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import AddUser from './components/Users/AddUser';
-
+import React from "react";
+import AddUser from "./components/Users/AddUser";
 
 function App() {
+  const newUserSubmit = (username, age) => {
+    const newUser = {
+      id: Math.random().toString(),
+      name: username,
+      age: age,
+    };
+
+    console.log(newUser);
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onUserSubmit={newUserSubmit} />
     </div>
   );
 }
