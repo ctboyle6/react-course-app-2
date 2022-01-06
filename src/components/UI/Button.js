c132cdc7d3931de4cf13ec0d04046ab9f6839cd2
Chris Boyle <cthboyle6@gmail.com>
2022-01-06 09:28:48 -0500

    connected Button UI to AddUser form component

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,11 +1,15 @@
-
+import styles from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <div>
+    <button
+      className={styles.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Button
+export default Button;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import styles from "./AddUser.module.css";
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -16,7 +17,7 @@ const AddUser = (props) => {
         <input id="username" type="text"></input>
         <label htmlFor="age">Age</label>
         <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
